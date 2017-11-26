01433ce24cf406f54153b94b906cab0923eeeaef
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-11-26 21:34:13 +0100

    Initial commit

---
+++ src/neptune.fs
@@ -0,0 +1,12 @@
+module Neptune
+
+open Fable.Core
+open Fable.Import
+open Fable.Import.vscode
+
+let activate (context : vscode.ExtensionContext) =
+    printfn "Hello world"
+
+    vscode.commands.registerCommand("extension.sayHello", fun _ ->
+        vscode.window.showInformationMessage "Hello world!" |> unbox )
+    |> context.subscriptions.Add
